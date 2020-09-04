4f207f19784a9e61710258878e0dd10ad50a087f
c332030 <c332030@gmail.com>
2020-09-04 12:34:36 +0800

    feat: PackageConstants

---
+++ c-commons-constant/src/main/java/com/c332030/constant/sys/PackageConstants.java
@@ -0,0 +1,20 @@
+package com.c332030.constant.sys;
+
+/**
+ * <p>
+ * Description: SysConstant
+ * </p>
+ *
+ * @author c332030
+ * @version 1.0
+ */
+public class PackageConstants {
+
+    /**
+     * 包名
+     */
+    public static final String BASE = "com.c332030";
+    public static final String BASE_DAO = "com.c332030.dao";
+    public static final String BASE_SERVICE = "com.c332030.service";
+
+}
