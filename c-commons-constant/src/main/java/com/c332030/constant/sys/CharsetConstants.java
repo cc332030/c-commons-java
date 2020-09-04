38af85545a28bd370bd96b878cf049870a01065a
c332030 <c332030@gmail.com>
2020-09-04 12:34:44 +0800

    feat: CharsetConstants

---
+++ c-commons-constant/src/main/java/com/c332030/constant/sys/CharsetConstants.java
@@ -0,0 +1,16 @@
+package com.c332030.constant.sys;
+
+import java.nio.charset.StandardCharsets;
+
+/**
+ * <p>
+ * Description: CharsetConstants
+ * </p>
+ *
+ * @author c332030
+ * @version 1.0
+ */
+public class CharsetConstants {
+
+    public static final String CHARSET_UTF8 = "charset=" + StandardCharsets.UTF_8;
+}
