80d8753fdb484622ddaf7f88f150edd0ac5c3daf
c332030 <c332030@gmail.com>
2021-07-12 11:04:40 +0800

    feat: CRuntimeException

---
+++ c-commons-model/src/main/java/com/c332030/model/exception/CRuntimeException.java
@@ -0,0 +1,13 @@
+package com.c332030.model.exception;
+
+/**
+ * <p>
+ * Description: CRuntimeException
+ * </p>
+ *
+ * @author c332030（袁兴旺）
+ * @version 1.0
+ */
+public class CRuntimeException extends RuntimeException {
+    private static final long serialVersionUID = -4549291214637484384L;
+}
