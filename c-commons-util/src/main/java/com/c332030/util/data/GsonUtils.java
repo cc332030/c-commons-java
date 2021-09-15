4c18c44351d6c39d608f4a33c1874b6577398a68
c332030 <c332030@gmail.com>
2021-09-15 09:46:35 +0800

    feat: GsonUtils public GSON

--- c-commons-util/src/main/java/com/c332030/util/data/GsonUtils.java
+++ c-commons-util/src/main/java/com/c332030/util/data/GsonUtils.java
@@ -22,7 +22,7 @@ import lombok.extern.slf4j.Slf4j;
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class GsonUtils {
 
-    private static final Gson GSON = new Gson();
+    public static final Gson GSON = new Gson();
 
     /**
      * <p>
